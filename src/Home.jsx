e0b63432fd1fad9012c904c485a2deada4283c2e
hselto <mselto@shutterstock.com>
2019-12-29 15:42:37 -0700

    react app bit

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Typography>Hello World</Typography>
+      </div>
+    );
+  }
+}
+
+export default Home;
