808fbfa1289f4e01ef6104972700d8cd9ca597ed
hselto <mselto@shutterstock.com>
2019-12-29 16:21:18 -0700

    weird fix for material

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <Typography>Hello World</Typography>
-      </div>
+      <Box>
+        <Typography>Hello World!</Typography>
+      </Box>
     );
   }
 }
