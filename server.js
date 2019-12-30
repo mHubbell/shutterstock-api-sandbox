f75e5a70cb383a639f29ce973bd162eeb4b6cc5a
hselto <mselto@shutterstock.com>
2019-12-30 00:55:32 -0700

    some commentary

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-// requires our private env params (api key and secret) and sets them for us
+/**
+ * This is the server side portion of our app.
+ * It sets up a simple express application (https://expressjs.com/)
+ * It also hooks into webpack in order to automatically restart the app
+ * as files change so that you dont need to restart it manually
+ * (you do need to refesh the browser however)
+ */
+// requires our private env params (api key and secret)
+// and sets them for us as environemnt variables
 require('./env.private');
+// main dependencies for the application
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
+// the shutterstock sdk (https://www.npmjs.com/package/shutterstock-api)
 const sstk = require('shutterstock-api');
+// local webpack configuration
 const config = require('./webpack.config.js');
 
+// create an express application (server)
 const app = express();
+// create a webpack compiler for packaging our front end javascript
 const compiler = webpack(config);
 
+// set up the shutterstock sdk with our key and secret
 sstk.setBasicAuth(
   process.env.SSTK_KEY,
   process.env.SSTK_SECRET,
 );
+// create a client for talking to the `ImagesApi` (basic image searching and interactions)
 const api = new sstk.ImagesApi();
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
@@ -21,6 +36,9 @@ app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
 }));
 
+// create a controller that will respond when we call our application
+// at the `/search` route. This for now just hardcodes a search and returns
+// its results.
 app.get('/search', async (req, res) => {
   const queryParams = {
     query: 'New York',
@@ -31,7 +49,7 @@ app.get('/search', async (req, res) => {
   res.json(results);
 });
 
-// Serve the files on port 3000.
+// Serve the application on port 3000.
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!\n');
 });
